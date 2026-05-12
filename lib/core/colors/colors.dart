237d3f777273c647ad9adff3301b5b03e085c2a2
Rifqi Rahmad Ichsan Makayasa Pratama <rifqimakayasa@gmail.com>
2026-05-12 23:20:59 +0700

    setup color

---
+++ lib/core/colors/colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+abstract class AppColors {
+  const AppColors._();
+
+  static const ink = Color(0xFF191919);
+  static const paper = Color(0xFFFFFFFF);
+  static const muted = Color(0xFF787774);
+  static const chip = Color(0xFFF1F1EF);
+  static const line = Color(0xFFE9E9E7);
+}
